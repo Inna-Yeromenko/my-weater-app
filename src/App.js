9bd852dc7de77ab48562c7e33b881048f2cad109
Inna-Yeromenko <i.yeryomenko2101@gmail.com>
2022-09-19 14:08:49 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Inna Yeromenko and is{" "}
+        <a
+          href="https://github.com/Inna-Yeromenko/my-weater-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
