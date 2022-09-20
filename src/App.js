80a7456bb1e23c8269e7b0de81d5745ee134fcec
Inna-Yeromenko <i.yeryomenko2101@gmail.com>
2022-09-20 11:19:53 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Inna Yeromenko and is{" "}
-        <a
-          href="https://github.com/Inna-Yeromenko/my-weater-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Inna Yeromenko and is{" "}
+          <a
+            href="https://github.com/Inna-Yeromenko/my-weater-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
