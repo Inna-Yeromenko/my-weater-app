8eabe79b7669d03c2c47fdff6f4a1e4eb55ea8da
Inna-Yeromenko <i.yeryomenko2101@gmail.com>
2022-09-21 16:02:07 +0300

    added axios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Kharkiv" />
         <footer>
           This project was coded by Inna Yeromenko and is{" "}
           <a
