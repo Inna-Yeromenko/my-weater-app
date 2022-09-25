234189e7d3f5dc2fbb9c644281a91b96496a7f15
Inna-Yeromenko <i.yeryomenko2101@gmail.com>
2022-09-25 19:15:53 +0300

    added clearfix

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,10 +13,14 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
-          <WeatherTemperature celsius={props.data.temperature} />
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
